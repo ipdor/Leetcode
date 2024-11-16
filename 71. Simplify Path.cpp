289e44dea970077ca0ddc5257f6cf249fcf0f408
fanyi.lu <nate.yoran@gmail.com>
2024-11-16 02:29:57 +0000

    Solved 71

--- 71. Simplify Path.cpp
+++ 71. Simplify Path.cpp
@@ -1,8 +1,40 @@
 //https://leetcode.com/problems/simplify-path/description/
+//也可以使用vector<string>作为栈，不过没有本质区别，最后也需要倒到res中
 class Solution {
 public:
     string simplifyPath(string path) 
     {
-        
+        path+="/";
+        string dir;
+        stack<string> st;
+        for(auto x: path)
+        {
+            if(x!='/')
+            {
+                dir+=x;
+            }
+            else if(dir!="")
+            {
+                //cout<<dir<<endl;
+                if(dir == "..")
+                {
+                    if(!st.empty())
+                        st.pop();
+                }
+                else if(dir !=".")
+                {
+                    st.push(dir);
+                }
+                dir="";
+            }
+
+        }
+        string res;
+        while(!st.empty())
+        {
+            res="/"+st.top()+res;
+            st.pop();
+        }
+        return res.size()?res:"/";
     }
 };
