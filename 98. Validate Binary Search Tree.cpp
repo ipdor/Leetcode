0cb75be8187b64c88e1b8cd7417193c677858c9c
fanyi.lu <nate.yoran@gmail.com>
2024-12-01 13:09:51 +0000

    Resolved 98

--- 98. Validate Binary Search Tree.cpp
+++ 98. Validate Binary Search Tree.cpp
@@ -10,27 +10,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//中序遍历，获得数组的递增/各数字递增说明是合格搜索树
+//最笨的办法是每个节点从上到下遍历一次，判断是否左边小右边大
 class Solution {
 public:
-    //是否子树所有值小于/大于value
-    bool judgeBST(TreeNode* root, int value, int greater)
+    int pre = INT_MIN;
+    bool first=true;
+    bool inOrder(TreeNode* root)
     {
-        if(!root)
-            return true;
-        //cout<< root->val <<" "<< value <<" "<< greater << endl;
-        if((greater && root->val <= value) || (!greater && root->val >= value))
+        if(root->left && inOrder(root->left) == false)
+            return false;
+        //cout<< root->val << " ";
+
+        if(first)
+            first=false;
+        else 
         {
+            if(root->val <= pre)
             return false;
         }
-        return judgeBST(root->left, value, greater) && judgeBST(root->right, value, greater);
+        pre = root->val;
+        if(root->right && inOrder(root->right) == false)
+            return false;
+        return true;
     }
     bool isValidBST(TreeNode* root) 
     {
-        if(!root)
-            return true;
-        bool res=1;
-        if(judgeBST(root->left, root->val, 0)==0 || judgeBST(root->right, root->val, 1)==0)
-            return false;
-        return isValidBST(root->left) && isValidBST(root->right);
+        return inOrder(root);
     }
 };
+/*
+class Solution {
+public:
+    void inOrder(TreeNode* root, vector<int> &res)
+    {
+        if(root->left) inOrder(root->left, res);
+        res.push_back(root->val);
+        if(root->right) inOrder(root->right, res);
+        return;
+    }
+    bool isValidBST(TreeNode* root) 
+    {
+        vector<int> res;
+        inOrder(root, res);
+        int pre=res[0];
+        for(int i = 1; i<res.size(); i++)
+        {
+            int x = res[i];
+            //cout<< x<< " ";
+            if(x<=pre)
+                return false;
+            pre = x;
+        }
+        //cout <<endl;
+
+        return true;
+    }
+};
+*/
