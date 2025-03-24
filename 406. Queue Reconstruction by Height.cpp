a484ae3b3b1e7a721928ce20434f93f96d02885b
fanyi.lu <nate.yoran@gmail.com>
2025-03-24 06:59:12 +0000

    Resolved 406

--- 406. Queue Reconstruction by Height.cpp
+++ 406. Queue Reconstruction by Height.cpp
@@ -5,26 +5,45 @@ bool comp(vector<int> &a, vector<int>&b)
 {
     return a[0]==b[0]? a[1]<b[1] : a[0]>b[0];
 }
+//list的插入效率O(1)，整体效率O(n)
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) 
     {
         sort(people.begin(), people.end(), comp);
-        vector<vector<int>> ans;
+        list<vector<int>> ans;
         for (size_t i = 0; i < people.size(); i++)
         {
-            int k = people[i][1], j=0;
-            cout<< k <<endl;
-            for (; j < ans.size()&& k>0; j++)
-            {
-                if (ans[j][0]>= people[i][0])
+            int k = people[i][1];
+            auto it = ans.begin();
+            for (; it!=ans.end() && k>0; it++) //可以直接循环k--，因为前面一定是高度大于等于它的
+                if ((*it)[0] >= people[i][0]) 
                     k--;
-            }
-            ans.insert(ans.begin()+j,people[i]);
-            // for (size_t i = 0; i < ans.size(); i++)
-            //     cout<< ans[i][0] <<","<< ans[i][1] << " ";
-            // cout<<endl;
+            ans.insert(it, people[i]);
         }
-        return ans;
+        return vector<vector<int>> (ans.begin(), ans.end());
     }
 };
+//vector版本，效率O(n2)
+// class Solution {
+// public:
+//     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) 
+//     {
+//         sort(people.begin(), people.end(), comp);
+//         vector<vector<int>> ans;
+//         for (size_t i = 0; i < people.size(); i++)
+//         {
+//             int k = people[i][1], j=0;
+//             for (; j < ans.size()&& k>0; j++)
+//             {
+//                 if (ans[j][0]>= people[i][0])
+//                     k--;
+//             }
+//             ans.insert(ans.begin()+j,people[i]);
+//             // for (size_t i = 0; i < ans.size(); i++)
+//             //     cout<< ans[i][0] <<","<< ans[i][1] << " ";
+//             // cout<<endl;
+//         }
+//         return ans;
+//     }
+// };
